add322140d0e3fac2a8460a0cd58fb9ccc98055b
Sam McDermott <samueldmcdermott@gmail.com>
2022-10-14 15:25:07 -0400

    style update for camb_ps_maker.py

--- camb_ps_maker.py
+++ camb_ps_maker.py
@@ -1,24 +1,11 @@
-import sys, platform, os
-import numpy as np
-
-import camb
-from camb import model, initialpower
-
+import sys, os, camb
 
 camb_dir = os.path.dirname(camb.__file__)
 
+print('Using CAMB %s installed at %s' % (camb.__version__, camb_dir))
 
-
-print('Using CAMB %s installed at %s'%(camb.__version__, camb_dir))
-
-
-
-pars=camb.read_ini(os.path.join(camb_dir,'inifiles', sys.argv[1]))
-
-
+pars = camb.read_ini(os.path.join(camb_dir, 'inifiles', sys.argv[1]))
 
 results = camb.get_results(pars)
 
-
-
-results.save_cmb_power_spectra("./"+sys.argv[2])
+results.save_cmb_power_spectra("./" + sys.argv[2])

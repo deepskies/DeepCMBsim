4be6377ac1e5952d61520648f9c174f14f0898de
Sam McDermott <samueldmcdermott@gmail.com>
2023-01-27 10:59:15 -0500

    corrected typo in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="code for simulating and delensing the cmb",
     packages=["simcmb"],
     url="https://github.com/deepskies/simcmb",
-    install_requires=["setuptools", "camb", "namaster", "astropy"],
+    install_requires=["setuptools", "camb", "pymaster", "astropy"],
     package_data={
         "simcmb": ["inifiles/*"],
     }

96a0da85f856852f83d9743a610eabca2fe42570
Sam McDermott <samueldmcdermott@gmail.com>
2023-04-13 11:05:42 -0500

    started making some changes that observe file refactoring discussed at code review

--- simcmb/camb_ps_maker.py
+++ simcmb/camb_ps_maker.py
@@ -1,34 +1,52 @@
 import os
-import re
 import camb
-import json
 import numpy as np
 from datetime import datetime as dt
 import h5py
-import yam_in as yi
 import noise
 
 """
-Code to create a single power spectrum or map from CAMB and/or namaster
-Uses the .ini file in inifiles/ as a baseline, changes r and A_lens
+Code to create an array of power spectra from CAMB based on a yaml file
 """
+
 class PS_Maker(object):
-    def __init__(self, infile = "planck_2018_1e4.yaml"):
-        self.infile = infile
-        self.Ydict = yi.Ydict(infile = self.infile)
+    def __init__(self, in_dict): #read in the yaml such that in_dict is yi.Ydict(infile) (this will be done in the ipynb)
+        self.Ydict = in_dict
+        self.Ydictu = self.Ydict.user_params #just an alias
 
         # according to the CAMB documentation, errors affect the last "100 or so" multipoles
-        self.max_l_use = min(self.Ydict.user_params['lmax_use'], noise.max_multipole(self.Ydict.user_params['beam_fwhm']) )
+        self.max_l_use = min(self.Ydictu['lmax_use'], noise.max_multipole(self.Ydictu['beam_fwhm']) )
 
-        self._outdir = self.Ydict.user_params['outfile_dir']
+        self._outdir = self.Ydictu['outfile_dir']
 
-        self.cls_raw, self.units = bool(self.Ydict.user_params['cls_raw']), self.Ydict.user_params['TT_dimension']
+        self.cls_raw, self.units = bool(self.Ydictu['cls_raw']), self.Ydictu['TT_dimension']
+
+    def get_noise(self):
+        if self.Ydictu['noise_type'] == 'white':
+            return noise.white_noise(self.Ydictu['noise_level'], self.Ydictu['beam_fwhm'], self.max_l_use, TT=True, units_uK=True), noise.white_noise(self.Ydictu['noise_level'], self.Ydictu['beam_fwhm'], self.max_l_use, TT=False, units_uK=False)
+        elif self.Ydictu['noise_type'] is None:
+            return 0.
+        else:
+            print("only white noise is currently implemented")
+            return 0.
+
+    def get_namestr(self, cpars):
+        namestr = "r" + f'{cpars.InitPower.r:0.2f}' + "_A" + f'{cpars.Alens:0.2f}' + "_lmax" + str(self.max_l_use) + "_noise" + str(self.Ydictu['noise_level']) +"." + str(self.Ydictu['beam_fwhm'])
+        if self.cls_raw:
+            namestr += "_rawCl"
+        return namestr
 
     def get_cls(self, cpars):
-        if bool(self.Ydict.user_params["verbose"]):
+        if bool(self.Ydictu["verbose"]):
             ta = dt.now()
         results = camb.get_results(cpars)
         tt, ee, bb, te = results.get_total_cls(raw_cl=self.cls_raw, CMB_unit=self.units).T
+        if self.Ydictu['noise_type'] is not None:
+            noise = self.get_noise()
+            tt += noise[0]
+            ee += noise[1]
+            bb += noise[1]
+            te += noise[1]
         pp, pt, pe = results.get_lens_potential_cls(raw_cl=self.cls_raw)[:self.max_l_use + 1].T
         lvals = range(self.max_l_use + 1)
         outarr = np.array([lvals, tt, ee, bb, te, pp, pt, pe])
@@ -36,9 +54,9 @@ class PS_Maker(object):
         outdict = {}
         for i in range(len(outlabs)):
             outdict[outlabs[i]] = outarr[i]
-        outdict['lensed_CLs'] = self.Ydict.user_params['FORCAMB']['DoLensing']
+        outdict['lensed_CLs'] = self.Ydictu['FORCAMB']['DoLensing']
 
-        if bool(self.Ydict.user_params["verbose"]):
+        if bool(self.Ydictu["verbose"]):
             tb = dt.now()
             print('from', dt.strftime(ta, '%H:%M:%S.%f %P'), 'to', dt.strftime(tb, '%H:%M:%S.%f %P'), end=" ")
             print('or', str((tb - ta).seconds) + '.' + str((tb - ta).microseconds), 'seconds total')
@@ -48,64 +66,23 @@ class PS_Maker(object):
 
         return outdict
 
-    def get_namestr(self, cpars):
-        namestr = "r" + f'{cpars.InitPower.r:0.2f}' + "_A" + f'{cpars.Alens:0.2f}' + "_lmax" + str(self.max_l_use) + "_noise" + str(self.Ydict.user_params['noise_level']) +"." + str(self.Ydict.user_params['beam_fwhm'])
-        if self.cls_raw:
-            namestr += "_rawCl"
-        outfilename = os.path.join(self._outdir, namestr)
-
-        return outfilename
-
     def loop_cls_rA(self, overwrite=False):
-        for rr in self.Ydict.rs:
+        for rr in self.Ydict.rs: #for par in self.Ydict['pass parameter']
             self.Ydict.pars.InitPower.r = rr
             for aa in self.Ydict.As:
                 self.Ydict.pars.Alens = aa
                 cpars_cur = self.Ydict.pars
                 if overwrite:
                     outdict, namestr = self.get_cls(cpars_cur), self.get_namestr(cpars_cur)
-                    self.savecls(outdict, namestr)
+                    savecls(outdict, os.path.join(self._outdir, namestr))
                 else:
                     namestr = self.get_namestr(cpars_cur)
                     if sum([namestr in x for x in os.listdir(self._outdir)])==0:
                         outdict, namestr = self.get_cls(cpars_cur), self.get_namestr(cpars_cur)
-                        self.savecls(outdict, namestr)
+                        savecls(outdict, os.path.join(self._outdir, namestr))
 
-    def saveflatmap(self):
-        if bool(self.j_data["verbose"]):
-            ta = dt.now()
-        import flatmaps as fm
-        from astropy.wcs import WCS
-        import pymaster as nmt
-        pixels = 192.  # 192 pixels on each side
-        side = 5  # 5 degrees on each side
-        reso = side / pixels
-        reso_arcmin = reso * 60
-        dx = reso * np.pi / 180.0  # Resolution in radians
-        lmax = 180. / reso  # Maximum l-mode achievable given these parameters
-        lstep = lmax * 2 / pixels
-        tfac = dx / pixels  # converts from pixels to radians
-        w = WCS(naxis=2)
-        nx = int(pixels)
-        ny = int(pixels)
-        w.wcs.crpix = [nx / 2, ny / 2]  # Center pixel X, Y
-        w.wcs.cdelt = np.array([-reso, reso])
-        w.wcs.crval = [0, 0]  # Center coordinates RA, DEC at 0,0
-        w.wcs.ctype = ["RA---AIR", "DEC--AIR"]  # Airy projection; can be adjusted. Previous used Azimuthal equal-area
-        fmi = fm.FlatMapInfo(w, nx=nx, ny=ny, lx=side, ly=side)
-        sim_map = nmt.synfast_flat(int(fmi.nx), int(fmi.ny), fmi.lx_rad, fmi.ly_rad, [self.tt], [0], seed=0)
-        np.save(self.outfilename, sim_map)
-        if bool(self.j_data["verbose"]):
-            tb = dt.now()
-            print('from', dt.strftime(ta, '%H:%M:%S.%f %P'), 'to', dt.strftime(tb, '%H:%M:%S.%f %P'), 'or', end=" ")
-            print(str((tb - ta).seconds) + '.' + str((tb - ta).microseconds), 'seconds total')
+def savecls(out_dict, out_name): #move this to yam_in.py
+    with h5py.File(out_name + '.h5', 'w') as f:
+        for k, v in out_dict.items():
+            f.create_dataset(k, data = v)
 
-    def savecls(self, odict, oname):
-        # np.savetxt(self.outfilename + '.txt', self.outarr,
-        #            header="originally written at " + dt.strftime(dt.now(), '%a, %b %d %Y, %I:%M:%S.%f %p') +
-        #                   # "\nusing log10(r) = " + f'{rr:0.2f}' + ", A = " + f'{aa:0.2f}' +
-        #                   '\nconfigured with json file ' + str(self.j_data)
-        #            )
-        with h5py.File(oname + '.h5', 'w') as f:
-            for k, v in odict.items():
-                f.create_dataset(k, data = v)
--- simcmb/yam_in.py
+++ simcmb/yam_in.py
@@ -4,7 +4,7 @@ import numpy as np
 from collections.abc import Iterable
 
 class Ydict(object):
-    def __init__(self, infile = "planck_2018_1e4.yaml"):
+    def __init__(self, infile = "planck_2018_1e4.yaml"): #put .yaml into a settings folder
         with open(infile, "r") as f:
             self.myyam = yaml.safe_load(f)
 
@@ -14,7 +14,7 @@ class Ydict(object):
         for x, y in self.basepars.items():
             try:
                 setattr(self.pars, x, y)
-            except Exception:
+            except Exception: #be more descriptive in the error type or do an assertion first
                 for a, b in y.items():
                     try:
                         setattr( getattr(self.pars, x), a, b)
@@ -32,17 +32,19 @@ class Ydict(object):
                     except Exception:
                         continue
 
-        rv = self.myyam['USERPARAMS']['rvals']
+        rv = self.myyam['USERPARAMS']['rvals'] #more descriptive name for r
         if (isinstance(rv, Iterable) and (len(rv)<=3)):
             rv[:2] = np.log10(rv[:2]) if (rv[0]>0) else rv[:2]
             self.rs = np.logspace(*rv)
         else:
             self.rs = np.array(rv)
 
-        Av = self.myyam['USERPARAMS']['Avals']
+        Av = self.myyam['USERPARAMS']['Avals'] #more descriptive name for A
         if (isinstance(Av, Iterable) and (len(Av)<=3)):
             self.As = np.linspace(*Av)
         else:
             self.As = np.array(Av)
 
         self.user_params = self.myyam['USERPARAMS']
+
+    #add a function to update parameters in self

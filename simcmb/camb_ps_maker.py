736e19b12aab6ff7a684794bd25a635a402c21c6
Sam McDermott <samueldmcdermott@gmail.com>
2023-05-03 14:02:44 -0400

    clarifying the code: better instance and dict naming in `yam_io.py`, clearer names in `camb_ps_maker.py`

--- simcmb/camb_ps_maker.py
+++ simcmb/camb_ps_maker.py
@@ -10,43 +10,53 @@ Code to create an array of power spectra from CAMB based on a yaml file
 includes noise, loops, and option to update parameters
 """
 
-class PS_Maker(object):
-    def __init__(self, in_dict): #read in the yaml such that in_dict is yi.Ydict(infile) (this will be done in the ipynb)
-        self.Ydict = in_dict
-        self.Ydictu = self.Ydict.all_params_dict['USERPARAMS']  # just an alias
+
+class PS_Maker:
+    def __init__(self, in_Ydict):  # read in the yaml such that in_Ydict is Ydict(infile)
+        self.Ydict = in_Ydict
+        self.CAMBparams = self.Ydict.CAMBparams
+        self.UserParams = self.Ydict.UserParams
 
         # according to the CAMB documentation, errors affect the last "100 or so" multipoles
-        self.max_l_use = int( min(self.Ydictu['max_l_use'], noise.max_multipole(self.Ydictu['beam_fwhm'])) )
-        self.Ydict.CAMBparams.max_l, self.Ydict.CAMBparams.max_l_tensor = int(self.max_l_use + self.Ydictu['extra_l']), int(self.max_l_use + self.Ydictu['extra_l'])
+        self.max_l_use = min(self.UserParams['max_l_use'], noise.max_multipole(self.UserParams['beam_fwhm']))
+        self.max_l_calc = int(self.max_l_use + self.UserParams['extra_l'])
+        self.CAMBparams.max_l = self.max_l_calc
+        self.CAMBparams.max_l_tensor = self.max_l_calc
         # could make this^ its own function so that it gets recalculated even when you update params
 
-        self._outdir = self.Ydictu['outfile_dir']
+        self._outdir = self.UserParams['outfile_dir']
 
-        self.cls_raw, self.units = bool(self.Ydictu['cls_raw']), self.Ydictu['TT_dimension']
+        self.cls_raw = bool(self.UserParams['cls_raw'])
+        self.units = self.UserParams['TT_dimension']
 
         self.results = {}  # initialize an empty dictionary that can be filled in later, if desired
 
     def get_noise(self):
-        if self.Ydictu['noise_type'] == 'white':
-            return noise.white_noise(self.Ydictu['noise_level'], self.Ydictu['beam_fwhm'], self.max_l_use, TT=True), noise.white_noise(self.Ydictu['noise_level'], self.Ydictu['beam_fwhm'], self.max_l_use, TT=False)
-        elif self.Ydictu['noise_type'] is None:
-            return np.zeros(self.max_l_use)
+        if self.UserParams['noise_type'] == 'white':
+            t_noise = noise.white_noise(self.UserParams['noise_level'], self.UserParams['beam_fwhm'], self.max_l_use,
+                                        TT=True)
+            eb_noise = noise.white_noise(self.UserParams['noise_level'], self.UserParams['beam_fwhm'], self.max_l_use,
+                                         TT=False)
+            return t_noise, eb_noise
+        elif self.UserParams['noise_type'] is None:
+            return np.zeros((2, self.max_l_use))
         else:
             print("only white noise is currently implemented")
-            return np.zeros(self.max_l_use)
+            return np.zeros((2, self.max_l_use))
 
     def get_namestr(self, cpars):
-        namestr = f"cls_camb_r{cpars.InitPower.r:0.2f}_A{cpars.Alens:0.2f}_lmax{self.max_l_use}_noise" + str(self.Ydictu['noise_level']) +"." + str(self.Ydictu['beam_fwhm'])
+        namestr = f"cls_camb_r{cpars.InitPower.r:0.2f}_A{cpars.Alens:0.2f}_lmax{self.max_l_use}_noise" + str(
+            self.UserParams['noise_level']) + "." + str(self.UserParams['beam_fwhm'])
         if self.cls_raw:
             namestr += "_rawCl"
         return namestr
 
-    def get_cls(self, cpars, save_to_dict = None):
-        if bool(self.Ydictu["verbose"]):
-            ta = dt.now()
-        results = camb.get_results(cpars)
+    def get_cls(self, save_to_dict=None):
+        if bool(self.UserParams["verbose"]):
+            time_start = dt.now()
+        results = camb.get_results(self.CAMBparams)
         tt, ee, bb, te = results.get_total_cls(raw_cl=self.cls_raw, CMB_unit=self.units)[:self.max_l_use + 1].T
-        if self.Ydictu['noise_type'] is not None:
+        if self.UserParams['noise_type'] is not None:
             noise = self.get_noise()
             tt += noise[0]
             ee += noise[1]
@@ -54,29 +64,36 @@ class PS_Maker(object):
             te += noise[1]
         pp, pt, pe = results.get_lens_potential_cls(raw_cl=self.cls_raw)[:self.max_l_use + 1].T
         lvals = range(self.max_l_use + 1)
-        outarr = np.array([lvals, tt, ee, bb, te, pp, pt, pe])
-        outlabs = ['l', 'clTT', 'clEE', 'clBB', 'clTE', 'clPP', 'clPT', 'clPE']
-        outdict = {}
-        for i in range(len(outlabs)):
-            outdict[outlabs[i]] = outarr[i]
+        outdict = {
+            'l': lvals,
+            'clTT': tt,
+            'clEE': ee,
+            'clBB': bb,
+            'clTE': te,
+            'clPP': pp,
+            'clPT': pt,
+            'clPE': pe
+        }
 
-        if bool(self.Ydictu["verbose"]):
-            tb = dt.now()
-            print('from', dt.strftime(ta, '%H:%M:%S.%f %P'), 'to', dt.strftime(tb, '%H:%M:%S.%f %P'), end=" ")
-            print('or', str((tb - ta).seconds) + '.' + str((tb - ta).microseconds), 'seconds total')
+        if bool(self.UserParams["verbose"]):
+            time_end = dt.now()
+            print('from', dt.strftime(time_start, '%H:%M:%S.%f %P'), 'to', dt.strftime(time_end, '%H:%M:%S.%f %P'),
+                  end=" ")
+            print('or', str((time_end - time_start).seconds) + '.' + str((time_end - time_start).microseconds),
+                  'seconds total')
 
         if save_to_dict is not None:
             self.results[save_to_dict] = outdict
         else:
             return outdict
 
-    def loop_sims(self, overwrite=False):#make this for general arguments!
+    def loop_sims(self, overwrite=False):
         # old version, only for r and Alens:
         # for rr in self.Ydict.rs: #for par in self.Ydict['pass parameter'] - use itertools.product!
-        #     self.Ydict.CAMBparams.InitPower.r = rr
+        #     self.CAMBparams.InitPower.r = rr
         #     for aa in self.Ydict.As:
-        #         self.Ydict.CAMBparams.Alens = aa
-        #         cpars_cur = self.Ydict.CAMBparams
+        #         self.CAMBparams.Alens = aa
+        #         cpars_cur = self.CAMBparams
         #         if overwrite:
         #             outdict, namestr = self.get_cls(cpars_cur), self.get_namestr(cpars_cur)
         #             savecls(outdict, os.path.join(self._outdir, namestr))#replace this with something other than save!
@@ -85,14 +102,14 @@ class PS_Maker(object):
         #             if sum([namestr in x for x in os.listdir(self._outdir)])==0:
         #                 outdict, namestr = self.get_cls(cpars_cur), self.get_namestr(cpars_cur)
         #                 savecls(outdict, os.path.join(self._outdir, namestr))
-        keys, values = list(self.Ydict.dict_iterables.keys()), self.Ydict.dict_iterables.values()
+        iterables = self.UserParams['ITERABLES']
+        keys, values = list(iterables.keys()), iterables.values()
         for vector in itertools.product(*values):
             for i in range(len(vector)):
                 self.Ydict.update_val(keys[i], vector[i])
-            self.get_cls(self.Ydict.CAMBparams, save_to_dict=zip(keys, vector))
+            self.get_cls(save_to_dict=zip(keys, vector))
 
         return self.results
-
 
 
 def savecls(all_sims, out_name, sims_to_save_start=None, sims_to_save_end=None, permission='r+', overwrite=False):
--- simcmb/yam_io.py
+++ simcmb/yam_io.py
@@ -12,40 +12,42 @@ def set_camb_attr(cambparams_instance, x, y):
         for a, b in y.items():
             try:
                 setattr(getattr(cambparams_instance, x), a, b)
-            except Exception:  # this is only necessary because of some weird type error with CustomSources.custom_source_ell_scales
+            except Exception:  # necessary because of error with CustomSources.custom_source_ell_scales
                 continue
 
 
-class Ydict(object):
+class Ydict:
     def __init__(self, infile="example_config.yaml"):  # put .yaml into a settings folder
         with open(infile, "r") as f:
-            self.all_params_dict = yaml.safe_load(f)
+            self._all_params_dict = yaml.safe_load(f)
 
         self.CAMBparams = camb.CAMBparams()  # creates a base CAMBparams instance
 
-        for x, y in self.all_params_dict['BASECAMBPARAMS'].items():  # these get set first and are potentially overwritten later
+        for x, y in self._all_params_dict['BASECAMBPARAMS'].items():  # get set first (potentially overwritten later)
             set_camb_attr(self.CAMBparams, x, y)
 
-        for x, y in self.all_params_dict['USERPARAMS']['FORCAMB'].items():
+        for x, y in self._all_params_dict['USERPARAMS']['FORCAMB'].items():
             set_camb_attr(self.CAMBparams, x, y)
 
-        for x, y in self.all_params_dict['USERPARAMS']['ITERABLES'].items():
-            if (isinstance(y, Iterable) and (len(y) <= 3)):
-                self.all_params_dict['USERPARAMS']['ITERABLES'][x] = np.linspace(*y)
+        for x, y in self._all_params_dict['USERPARAMS']['ITERABLES'].items():
+            if isinstance(y, Iterable) and len(y) <= 3:
+                self._all_params_dict['USERPARAMS']['ITERABLES'][x] = np.linspace(*y)
             else:
                 if not isinstance(y, Iterable):
                     print(x, "is not iterable; are you sure it should be in ITERABLES?")
-                self.all_params_dict['USERPARAMS']['ITERABLES'][x] = np.array(y)
+                self._all_params_dict['USERPARAMS']['ITERABLES'][x] = np.array(y)
 
-        self.dict_iterables = self.all_params_dict['USERPARAMS']['ITERABLES']  # want to make this more easily accessible, for loops
+        self.UserParams = self._all_params_dict['USERPARAMS']
+
+        self.dict_iterables = self._all_params_dict['USERPARAMS']['ITERABLES']  # make this more easily accessible
 
     def update_val(self, attr, new_val):
         attr_split = re.split("\.", attr)
         if (len(attr_split) == 1) and (hasattr(self.CAMBparams, attr)):
             setattr(self.CAMBparams, attr, new_val)
-        elif (len(attr_split) == 2) and (hasattr( getattr(self.CAMBparams, attr_split[0]), attr_split[1])):
+        elif (len(attr_split) == 2) and (hasattr(getattr(self.CAMBparams, attr_split[0]), attr_split[1])):
             setattr(getattr(self.CAMBparams, attr_split[0]), attr_split[1], new_val)
-        elif attr in self.all_params_dict['USERPARAMS']:
-            self.all_params_dict['USERPARAMS'][attr] = new_val
+        elif attr in self._all_params_dict['USERPARAMS']:
+            self._all_params_dict['USERPARAMS'][attr] = new_val
         else:
             print("not a valid attribute")

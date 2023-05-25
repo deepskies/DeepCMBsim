4ae343d39b9f8dbefcec9dccdb3ffeeaf14474c8
Sam McDermott <samueldmcdermott@gmail.com>
2023-05-25 15:14:13 -0400

    first test added in `test_yam_io.py`

--- tests/test_yam_io.py
+++ tests/test_yam_io.py
@@ -1,6 +1,12 @@
 """
-tests camb_ps_maker.py
+tests yam_io.py
 """
 
-def __init__():
-    pass
+from simcmb.yam_io import config_obj
+
+
+def test_update_val():
+    co = config_obj()
+    co.update_val("Alens", 10)
+    co.update_val("InitPower.r", 10)
+    assert [co.CAMBparams.Alens, co.CAMBparams.InitPower.r] == [10, 10]
